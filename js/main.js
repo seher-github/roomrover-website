534f5cb9d853c127095e459065a5186cf065c8c0
seher-github <157596107+seher-github@users.noreply.github.com>
2024-05-28 17:51:11 +0500

    updated

--- js/main.js
+++ js/main.js
@@ -89,6 +89,47 @@
             }
         }
     });
+
+    // Booking Form
+    $("#submitbtn").click(function(event){
+        event.preventDefault(); // Prevent the default form submission
+    
+        var name = $("#name").val();
+        var lastname = $("#lastname").val();
+        var email = $("#email").val();
+        var no = $("#no").val();
+        var datetime = $("#datetime").val();
+        var select1 = $("#select1").val();
+        var message = $("#message").val();
+    
+        var formData = {
+            name: name,
+            lastname: lastname,
+            email: email,
+            no: no,
+            datetime: datetime,
+            select1: select1,
+            message: message
+        };
+    
+        $.ajax({
+            url: 'https://jsonplaceholder.typicode.com/posts',
+            type: 'POST',
+            contentType: 'application/json',
+            data: JSON.stringify(formData),
+            success: function(response) {
+                $("#response").html("<div class='alert alert-success'>Hi!"+response.name+" On Your Request "+response.select1+" is reserved now</div>");
+            },
+            error: function(xhr, status, error) {
+                if (xhr.status == 404) {
+                    $("#response").html("<div class='alert alert-danger'>File not found</div>");
+                } else {
+                    $("#response").html("<div class='alert alert-danger'>An error occurred: " + error + "</div>");
+                }
+            }
+        });
+    });
+    
     
 })(jQuery);
 
